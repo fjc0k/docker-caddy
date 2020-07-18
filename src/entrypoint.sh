42a6e580cf090832cfa1300798e6291f9338ebfc
Jay Fong <fjc0kb@gmail.com>
2020-07-19 05:26:17 +0800

    支持 host.docker.internal；解决 ocsp 被污染问题 (#1)

---
+++ src/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -ex
+
+# 支持 host.docker.internal
+echo -e "`/sbin/ip route | awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
+
+# 解决 ocsp 被污染问题
+echo -e "${OCSP_IP:-23.215.132.184}\tocsp.int-x3.letsencrypt.org" >> /etc/hosts
+
+caddy run
