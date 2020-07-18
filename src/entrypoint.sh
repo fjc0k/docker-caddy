a2d50432fd54b842d36efe86be67bf9c81010fe2
Jay Fong <fjc0kb@gmail.com>
2020-07-19 04:52:29 +0800

    up

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,6 +5,6 @@ set -ex
 # dockerhost
 echo -e "`/sbin/ip route | awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
 
-echo "${OCSP_IP:-23.215.132.184}\tocsp.int-x3.letsencrypt.org" >> /etc/hosts
+echo -e "${OCSP_IP:-23.215.132.184}\tocsp.int-x3.letsencrypt.org" >> /etc/hosts
 
 caddy run
